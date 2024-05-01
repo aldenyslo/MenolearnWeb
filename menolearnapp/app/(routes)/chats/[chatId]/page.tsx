772318587c7f937cc5e2cd1fa1e2f5be9c21b308
alden3721 <alden.ys.lo@gmail.com>
2024-04-30 20:49:14 -0700

    prisma generate script

--- menolearnapp/app/(routes)/chats/[chatId]/page.tsx
+++ menolearnapp/app/(routes)/chats/[chatId]/page.tsx
@@ -6,7 +6,6 @@ const Chat = async ({
 }: {
   params: { chatId: string }
 }) => {
-  //   const messages = await getMessages(params.chatId)
   return (
     <main className="bg-secondary-200 relative font-inter flex flex-col w-[390px] h-[812px] border border-t-0  border-red-200">
       <ChatLayout />
